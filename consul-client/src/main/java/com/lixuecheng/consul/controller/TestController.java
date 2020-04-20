a74df7965b5e6aa8003eac95a87cef09c6f20c8d
liwuhui-cheng <1652853513@qq.com>
2020-04-20 21:30:32 +0800

    初始化项目

---
+++ consul-client/src/main/java/com/lixuecheng/consul/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.lixuecheng.consul.controller;
+
+import com.lixuecheng.consul.config.ConsulConfigInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RefreshScope动态刷新配置
+@RefreshScope
+@RestController
+public class TestController {
+    @Value("${config.info}")
+    private String configInfo;
+    @RequestMapping("getConfig")
+    public Object getConfig(){
+        return configInfo;
+    }
+
+    @Autowired
+    private ConsulConfigInfo consulConfigInfo;
+
+    @RequestMapping("gainConfig")
+    public Object gainConfig(){
+        return consulConfigInfo;
+    }
+}
